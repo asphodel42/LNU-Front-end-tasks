5c4d16cc8b8112099353560adcec8baa5a13f024
asphodel42 <asphodell42@gmail.com>
2025-02-19 23:49:14 +0200

    added using of json file

--- Lab1-admin-pannel/index.js
+++ Lab1-admin-pannel/index.js
@@ -5,13 +5,16 @@ const app = express();
 const localhost = '127.0.0.1';
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/src/data', express.static(path.join(__dirname, 'src/data')));
+
 app.get('/', (req, res) =>{
   res.sendFile(path.join(__dirname, '/public/form/form.html'));
-})
+});
 
 app.get('/admin', (req, res)=>{
   res.sendFile(path.join(__dirname, '/public/admin/admin.html')); 
-})
+});
 
 app.listen(port, localhost, ()=>{
   console.log(`Server is running on http://${localhost}:${port}`);
